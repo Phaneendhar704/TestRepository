7f3e23359a2262f5e0f32696d36643c3a32d0594
Phani <Phani@LAPTOP-PUHP1I92>
2020-08-19 08:52:52 +0530

    assessment done

--- src/main/java/com/example/TestDemo/TestDemoApplication.java
+++ src/main/java/com/example/TestDemo/TestDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestDemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("print");
+		System.out.println("print phani assessment");
 		SpringApplication.run(TestDemoApplication.class, args);
 	}
 
